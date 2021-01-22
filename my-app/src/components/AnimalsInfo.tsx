84845b240cf42bb8cf5f14ac5e91fcd8b3bc6111
Nattie <usolnatalia@gmail.com>
2021-01-23 12:30:11 +1300

    Add enrichments

--- my-app/src/components/AnimalsInfo.tsx
+++ my-app/src/components/AnimalsInfo.tsx
@@ -1,5 +1,5 @@
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
-import { ColDef, DataGrid } from '@material-ui/data-grid';
+import { ColDef} from '@material-ui/data-grid';
 import React, { useEffect, useState } from 'react';
 import './Search.css';
 
@@ -50,18 +50,36 @@ interface SharedHabitat {
     animal:string
 }
 
+interface Enrichment{
+    name:string,
+    url:string,
+    cost:string,
+    type:string,
+    animals:Species[]
+}
+
+interface Species{
+    species:string
+}
+
 interface IMediaGridProps {
     SearchQuery: (string|null)
 }
 function AnimalsInfo(props:IMediaGridProps) {
-    var JSON_data = require('../JSON components/animals.json');
+    var JSON_animals = require('../JSON components/animals.json');
     const [animalInformation, setAnimalInformation] = useState<Animals[]>([{species:"", interactivity:"",social:{group_size:"", male:"", female:""},reproduction:{maturity:0,incubation:0,interbirth:0},continents:"", conversation_status:"",habbitat:{land_area:"", land_area_for_additional_animal:"", water_area:"", water_area_for_additional_animal:"", climbing_area:"", climbing_areay_for_additional_animal:"", temperature:"", humidity:"", biomes:[{biome:""}]}, url:"", id:0, population:"", edition:"", shared_habitat:[{animal:""}] }]);
 
+    var JSON_habbitat = require('../JSON components/habitat.json');
+    const [habitatInformation, setHabitatInformation] = useState<Enrichment[]>([{name:"", url:"", cost:"",type:"", animals:[{species:""}]}]);
+
+    const [toggleTable, setToggleTable] = useState(true);
+
     useEffect(()=> {
-        setAnimalInformation(JSON_data);
+        setAnimalInformation(JSON_animals);
+        setHabitatInformation(JSON_habbitat);
 
     // eslint-disable-next-line
-    }, [JSON_data]);
+    }, [JSON_animals,JSON_habbitat]);
 
     for(var i=0; i<animalInformation.length; i++){
         if(animalInformation[i].edition === "standard" ){
@@ -84,38 +102,70 @@ function AnimalsInfo(props:IMediaGridProps) {
         }
     }
 
-    const columns:ColDef[]=[
-        {
-            field:"species", headerName:"Species", width:300,headerAlign: 'center',
-        },
-        {
-            field:"conversation_status", headerName:"Conservative Status", width:300,headerAlign: 'center',
-        },
-        {
-            field:"continents", headerName:"Continents", width:300,headerAlign: 'center',
-        },
-        {
-            field:"edition", headerName:"Edition", width:300,headerAlign: 'center',
-        },
-        {
-            field:"population", headerName:"Population", width:300,headerAlign: 'center',
-        }
-    ];
-
+    function returnTable(){
+        var body = (
+            <TableContainer>
+                <Table  aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Species</TableCell>
+                        <TableCell align="center" sortDirection="asc">Conservative Status</TableCell>
+                        <TableCell align="center">Continents</TableCell>
+                        <TableCell align="center">Edition</TableCell>
+                        <TableCell align="center">Population</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {animalInformation.map((row) => (
+                        <TableRow key={row.species}>
+                            <TableCell component="th" scope="row" >
+                                {row.species}
+                            </TableCell>
+                            <TableCell align="center">{row.conversation_status}</TableCell>
+                            <TableCell align="center">{row.continents}</TableCell>
+                            <TableCell align="center">{row.edition}</TableCell>
+                            <TableCell align="center">{row.population}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+        return body;
+    }
     function getResult(){
         var index = animalInformation.findIndex((item,i)=> {
             return item.species === props.SearchQuery
         });
         var body;
 
-        console.log(index);
-        console.log(animalInformation[index].interactivity);
+        var newToyList = [];
+        var newFoodList = [];
+        for(var i=0; i<habitatInformation.length;i++){
+           for(var j=0; j<habitatInformation[i].animals.length;j++){
+                if(habitatInformation[i].animals[j].species === animalInformation[index].species){
+                    if(habitatInformation[i].type==="toy"){
+                        newToyList.push({
+                            name:habitatInformation[i].name,
+                            url:habitatInformation[i].url
+                        })
+                    }
+                    else if(habitatInformation[i].type==="food"){
+                        newFoodList.push({
+                            name:habitatInformation[i].name,
+                            url:habitatInformation[i].url
+                        })
+                    }
+                }
+           }
+        }
+        
         if(animalInformation[index].interactivity === "full"){
             body = (
                 <div>
                     <div className="DivToMakeFirstRow">
-                        <div>
-                            <img src={animalInformation[index].url} width="40%"/>
+                        <div className="FirstRowLeft">
+                            <img src={animalInformation[index].url} height={200} className="AnimalImage"/>
                             <div>
                                 <h3>{animalInformation[index].species}</h3>
                                 {animalInformation[index].habbitat.land_area === "0"?null:<p>Land area: {animalInformation[index].habbitat.land_area} m<sup>2</sup> (For every additional animal: {animalInformation[index].habbitat.land_area_for_additional_animal} m<sup>2</sup>)</p>}
@@ -129,11 +179,16 @@ function AnimalsInfo(props:IMediaGridProps) {
                         <div>
                             <p>Continents: {animalInformation[index].continents}</p>
                             <p>Biomes: {animalInformation[index].habbitat.biomes.map((item,i) => <span key={i}><br/>{item.biome}</span>)}</p>
+                            <p>Population in wild: {animalInformation[index].population}</p>
+                            <p>Temperature of Habitat: {animalInformation[index].habbitat.temperature} °C</p>
                         </div>
                     </div>
                     <div className="DivToMakeSecondRow">
-                        <div>
-    
+                        <div className="ToyEnrichment">
+                            {newToyList.map((item,i) => <span key={i}><br/><img src={item.url} width={50}/>{item.name}</span>)}
+                        </div>
+                        <div className="FoodEnrichment">
+                            {newFoodList.map((item,i) => <span key={i}><br/><img src={item.url} width={50}/>{item.name}</span>)}
                         </div>
                     </div>
                 </div>
@@ -164,32 +219,9 @@ function AnimalsInfo(props:IMediaGridProps) {
     }
     return (
         <div>
-            {props.SearchQuery===""? <TableContainer>
-                <Table  aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Species</TableCell>
-                        <TableCell align="center" sortDirection="asc">Conservative Status</TableCell>
-                        <TableCell align="center">Continents</TableCell>
-                        <TableCell align="center">Edition</TableCell>
-                        <TableCell align="center">Population</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {animalInformation.map((row) => (
-                        <TableRow key={row.species}>
-                        <TableCell component="th" scope="row" >
-                            {row.species}
-                        </TableCell>
-                        <TableCell align="center">{row.conversation_status}</TableCell>
-                        <TableCell align="center">{row.continents}</TableCell>
-                        <TableCell align="center">{row.edition}</TableCell>
-                        <TableCell align="center">{row.population}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>: getResult()}
+            <div>
+            {props.SearchQuery === ""? returnTable() : getResult()}
+        </div>
         </div>
     );
 }
